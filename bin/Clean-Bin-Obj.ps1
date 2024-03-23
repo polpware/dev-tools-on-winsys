12cc668765e526f0422726d2f5bbfce4221b6130
Tom T <polpware@gmail.com>
2024-03-23 18:14:27 -0400

    Command

--- bin/Clean-Bin-Obj.ps1
+++ bin/Clean-Bin-Obj.ps1
@@ -1,12 +1,12 @@
 Write-Host -ForegroundColor DarkBlue -BackgroundColor Green "Searching for bin and obj in the folder ..."
 
-Get-Location | Get-ChildItem -Depth 1 -Directory -Include bin,obj
+Get-Location | Get-ChildItem -Depth 1 -Directory -Include bin,obj | where {$_ -notmatch 'node_modules' -and $_ -notmatch 'wwwroot' -and $_ -notmatch '.git'} 
 
 $answer = Read-Host -Prompt 'Are you sure to delete them? (y for yes)'
 
 If ($answer -eq "y") {
   Write-Host -ForegroundColor DarkBlue -BackgroundColor Green "Starting to delete them ..."
-  Get-Location | Get-ChildItem -Depth 1 -Directory -Include bin,obj | Remove-Item -Recurse
+  Get-Location | Get-ChildItem -Depth 1 -Directory -Include bin,obj | where {$_ -notmatch 'node_modules' -and $_ -notmatch 'wwwroot' -and $_ -notmatch '.git'} | Remove-Item -Recurse
   Write-Host -ForegroundColor DarkBlue -BackgroundColor Green "Done"
 } Else {
   Write-Host -ForegroundColor DarkBlue -BackgroundColor Green "Done without doing nothing"
